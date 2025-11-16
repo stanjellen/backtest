1d4da82a496eefebc076eba1b1013742a58cc2db
Stan Jellen <stanj@tbadigital.com>
2025-11-16 10:37:13 -0800

    db f'n, rename $R, instantStop untested, include/

--- index.php
+++ index.php
@@ -1,29 +1,58 @@
 <style>
-    .win { color: darkgreen; }
-    .loss { color: darkred; }
-    </style>
+    .win {
+        color: darkgreen;
+    }
+
+    .loss {
+        color: darkred;
+    }
+</style>
+<pre>
 <?php
-// require_once 'db.php';
-$query = <<<SQL
-    SELECT *, UNIX_TIMESTAMP(CONVERT_TZ(minute, 'UTC', 'America/New_York')) as time from minute
-    WHERE symbol = ?
-        -- Regular trading hours only
-        AND TIME(`minute`) >= '09:30' AND TIME(`minute`) < '16:00'
-    ORDER BY minute ASC
-    SQL;
-$stuff = psquery($query, 'TSLA');
-$data = $stuff->fetch_all(MYSQLI_ASSOC);
+require_once 'include/db.php';
+// $data = db_minutes('TSLA', extended: false, fromDate: '$data[] = $row;2025-01-01', toDate: '2025-06-13', debug: true);
+// first date in CSV file
+$data2 = db_minutes('TSLA', extended: true, fromDate: '2025-01-02', toDate: '2025-01-02', debug: true);
+// first market open in DST - started at 0400
+// $data2 = db_minutes('TSLA', extended: true, fromDate: '2025-03-10', toDate: '2025-03-10', debug: true);
+// Monday May 12, 2025
+// $data2 = db_minutes('TSLA', extended: true, fromDate: '2025-05-12', toDate: '2025-05-12', debug: true);
 
 // old from csv file
-// $file = "../data/TSLA.USUSD_Candlestick_1_M_BID_01.01.2025-14.06.2025_NOFLATS.csv";
-// $headings = ['time', 'open', 'high', 'low', 'close', 'volume'];
-// $data = get_csv($file, $headings, ignore_first_row: true);
-// format_data($data);
+$file = "../data/TSLA.USUSD_Candlestick_1_M_BID_01.01.2025-14.06.2025_NOFLATS.csv";
+$headings = ['time', 'open', 'high', 'low', 'close', 'volume'];
+$data = get_csv($file, $headings, ignore_first_row: true);
+format_data($data);
+
+
+
+$dbLen = count($data2);
+$csvLen = count($data);
+echo "DB rows: $dbLen, CSV rows: $csvLen<br />\n";
+
+
+// compare datasets
+$i = 0;
+$dbRow = $data2[$i];
+$csvRow = $data[17140];
+
+echo "DB ROW:<br />\n";
+debug($dbRow);
+
+echo "CSV ROW:<br />\n";
+debug($csvRow);
+echo "<br />\n";
+
 
 // expects minute bars - no premarket
 // tracks day open, day high/low, 5-min open range
 
 date_default_timezone_set('America/New_York');
+
+debug("DB TIME: " . date("Y-m-d H:i", $dbRow['time']));
+debug("CSV TIME: " . date("Y-m-d H:i", $csvRow['time']));
+die;
+
 
 
 // todo week high/low
@@ -57,7 +86,7 @@ $indent = "               ";
 $trueRanges = []; // for ATR calculation. ignore market open gaps, reset each day
 foreach ($data as $i => $bar) {
     $bar['i'] = $i;
-    $ymd= date("Y-m-d", $bar['time']);
+    $ymd = date("Y-m-d", $bar['time']);
     $dow = date("D", $bar['time']);
     $ftime = date("H:i", $bar['time']);
     $rangeLabel = labelRange($bar['close'], $prevDaybar);
@@ -78,7 +107,7 @@ foreach ($data as $i => $bar) {
             $retest5h = false;
             $bnr5l = false;
             $retestDone = false;
-        } 
+        }
         $trueRanges = [true_range($bar, null)];
         // echo date("Y-m-d D ", $bar['time']) . " $rangeLabel\n";
     } else {
@@ -113,6 +142,7 @@ foreach ($data as $i => $bar) {
     }
     if (!$retest5h) continue;
 
+    // 3) Entry logic ----------------------------------------------------------------------
     // wait for next bar after retest
     // bars since retest
     if (($bsr = $i - $retest5h['i']) === 0) continue;
@@ -127,17 +157,17 @@ foreach ($data as $i => $bar) {
             $entryBar = $bar;
             $entryPrice = $bar['close'];
             // $SL = $retest5h['low'] - 0.10; // 10 cents below retest low
-            $SL = $retest5h['low'];
-            $SLAmount = round($entryPrice - $SL, 2);
+            $SL = $retest5h['low']; // Bar close stop loss
+            $R = round($entryPrice - $SL, 2);
             $plHOD = round($hodBar['high'] - $entryPrice, 2);
             echo ' ATR=' . round(ATR(), 2) . "\n";
             // take profit logic
-            // $TP = $entryPrice + max($plHOD, 2 * $SLAmount); // max(2R, HOD),
-            // $TP = $entryPrice + min($plHOD, 2 * $SLAmount); // min(2R, HOD), Worse than 2R
-            // $TP = $entryPrice + (2 * $SLAmount);
-            $TP = $entryPrice + min(max($plHOD, 2 * $SLAmount), 3*ATR()); // max(2R, HOD)  capped at 3*ATR - best so far. P&L 17.38
+            // $TP = $entryPrice + max($plHOD, 2 * $R); // max(2R, HOD),
+            // $TP = $entryPrice + min($plHOD, 2 * $R); // min(2R, HOD), Worse than 2R
+            // $TP = $entryPrice + (2 * $R);
+            $TP = $entryPrice + min(max($plHOD, 2 * $R), 3 * ATR()); // max(2R, HOD)  capped at 3*ATR - best so far. P&L 17.38
             $targetGain = round($TP - $entryPrice, 2);
-            $RR = round($targetGain / $SLAmount, 2);
+            $RR = round($targetGain / $R, 2);
             $position = [
                 'entryBar' => $bar,
                 'size' => 4,
@@ -145,7 +175,7 @@ foreach ($data as $i => $bar) {
             ];
 
 
-            echo "$ymd $dow $ftime <b>Entry - Size: {$position['size']} price: {$entryPrice} SL: $SL TP: $TP Gain: $targetGain, SL Amount: $SLAmount, R/R: $RR</b>\n";
+            echo "$ymd $dow $ftime <b>Entry - Size: {$position['size']} price: {$entryPrice} SL: $SL TP: $TP Gain: $targetGain, SL Amount: $R, R/R: $RR</b>\n";
         } else {
             // done for the day for now
             // todo better logic
@@ -166,15 +196,15 @@ foreach ($data as $i => $bar) {
 
     $barsSinceEntry = $i - $entryBar['i'];
 
-    // 3) take profit / Stop loss
+    // 4) Exit logic ----------------------------------------------------------------------
     // take profit
     if ($TP <= $bar['high']) {
         $sellSize = ceil($position['size'] / 2);
         $position['size'] -= $sellSize;
         $winAmount = round(($TP - $entryPrice) * $sellSize, 3);
         $position['realized'] += $winAmount;
-        $SL += $SLAmount; // move stop loss to breakeven
-        $TP += $SLAmount; // move take profit up by 1R
+        $SL += $R; // move stop loss to breakeven
+        $TP += $R; // move take profit up by 1R
         if ($position['size']) {
             echo "$indent<b class=\"win\">$barsSinceEntry mins Scaled out. remaining size: {$position['size']} Realized: $winAmount</b>\n";
         } else {
@@ -184,10 +214,14 @@ foreach ($data as $i => $bar) {
             $retestDone = true;
         }
     }
-    // stop loss at bar close
-    if ($SL >= $bar['close']) {
-        
-        $realized = round($position['size'] * ($bar['close'] - $entryPrice), 2);
+    // stop loss
+    // $isInstantStop = $bar['low'] <= $IntraBarSL;
+    $isInstantStop = false;
+    $IntraBarSL = $SL - $R; // Immediate Stop at SL - 1R
+    $isBarStop = $bar['close'] <= $SL;
+    if ($isInstantStop || $isBarStop) {
+        $exitPrice = $isInstantStop ? $IntraBarSL : $SL;
+        $realized = round($position['size'] * ($exitPrice - $entryPrice), 2);
         $position['realized'] += $realized;
         if ($position['realized'] >= 0) {
             // todo check if actually a win. bar could close below breakeven SL
@@ -324,3 +358,51 @@ function get_csv($file, $header = NULL, $ignore_first_row = false) {
 //     $table = 'minute';
 //     return 
 // }
+
+function db_minutes($symbol, $extended = false, $fromDate = null, $toDate = null, $debug = false) {
+    $where_sql = implode(
+        "\n",
+        array_filter([
+            "symbol = ?",
+            $extended ? "" : " AND TIME(`minute`) >= '09:30' AND TIME(`minute`) < '16:00'",
+            $fromDate ? " AND DATE(`minute`) >= ?" : "",
+            $toDate ? " AND DATE(`minute`) <= ?" : "",
+        ])
+    );
+    $values = [
+        $symbol,
+        // regular hours clause needs no value
+        ...$fromDate ? [$fromDate] : [],
+        ...$toDate ? [$toDate] : [],
+    ];
+    $query = <<<SQL
+        -- SELECT *, UNIX_TIMESTAMP(CONVERT_TZ(minute, 'UTC', 'America/New_York')) as time from minute
+        SELECT *, UNIX_TIMESTAMP(minute) as time from minute
+        WHERE $where_sql
+        ORDER BY minute ASC
+        SQL;
+
+    if ($debug) debug($query,'Query VALUES', $values);
+
+    $result = psquery($query, ...$values);
+    return $result->fetch_all(MYSQLI_ASSOC);
+}
+
+function debug($var){
+    if (func_num_args() > 1){
+        $args = func_get_args();
+        foreach ($args as $v){
+            debug($v);
+        }
+        return;
+    }
+    echo '<pre>';
+    if (!isset($var)){
+        echo "NULL";
+    }else if (is_array($var) || is_object($var)){
+        print_r($var);
+    }else{
+        echo htmlentities($var,ENT_QUOTES,'utf-8');
+    }
+    echo '</pre>';
+}
